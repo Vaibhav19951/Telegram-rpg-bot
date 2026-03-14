83a26ef04e66145e44b4f2a6a622ba4225a1cbac
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-14 22:33:09 +0530

    Update guild_system.py

--- systems/guild_system.py
+++ systems/guild_system.py
@@ -1,5 +1,14 @@
-
 guilds = {}
 
-def create_guild(name, leader):
-    guilds[name] = {"leader":leader,"members":[leader]}
+def create_guild(name, owner):
+    if name in guilds:
+        return False
+    guilds[name] = {"owner": owner, "members":[owner]}
+    return True
+
+def join_guild(name, member):
+    if name not in guilds:
+        return False
+    if member not in guilds[name]['members']:
+        guilds[name]['members'].append(member)
+    return True
