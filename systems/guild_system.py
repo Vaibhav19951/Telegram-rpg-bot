1b1f206c1efc38d3edcaf48cbfa58629f30b705d
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-14 22:34:04 +0530

    Update guild_system.py

--- systems/guild_system.py
+++ systems/guild_system.py
@@ -1,17 +1,19 @@
+import random
 from database.db import load_players, save_players
 players = load_players()
 
-def add_item(player_id, item):
-    player = players.get(str(player_id), {"inventory":[], "gold":100})
-    player['inventory'].append(item)
-    players[str(player_id)] = player
+def pvp_attack(player1_id, player2_id):
+    p1 = players.get(str(player1_id), {"HP":100, "Attack":10})
+    p2 = players.get(str(player2_id), {"HP":100, "Attack":10})
+
+    p1_attack = p1['Attack'] + random.randint(0,5)
+    p2_attack = p2['Attack'] + random.randint(0,5)
+
+    p1['HP'] -= p2_attack
+    p2['HP'] -= p1_attack
+
+    players[str(player1_id)] = p1
+    players[str(player2_id)] = p2
     save_players(players)
 
-def remove_item(player_id, item):
-    player = players.get(str(player_id), {"inventory":[]})
-    if item in player['inventory']:
-        player['inventory'].remove(item)
-        players[str(player_id)] = player
-        save_players(players)
-        return True
-    return False
+    return {"player1_hp": p1['HP'], "player2_hp": p2['HP'], "p1_dealt": p1_attack, "p2_dealt": p2_attack}
