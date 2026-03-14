8d0b274398a3120d237f38d63ff42ce2476859fb
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-14 18:30:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from commands.guild import createguild, joinguild
 from commands.aura import aura
 from commands.shop import shop
 
-TOKEN = "8128908243:AAHFNrzMrS_FBT_sSF4AUQlIypK0vIzLARo"
+TOKEN = "8128908243:AAFMZZZF6JAvXnCJPStaFt6hpxeyGPH5FMI"
 
 logging.basicConfig(level=logging.INFO)
 
