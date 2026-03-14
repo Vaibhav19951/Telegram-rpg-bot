981cee8d93f5259c52dafdf4281403e6227672a5
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-14 22:28:49 +0530

    Update auras.py

--- data/auras.py
+++ data/auras.py
@@ -1,7 +1,6 @@
-
 auras = {
-"neutral":{"damage":5},
-"fire":{"damage":20},
-"power":{"damage":30},
-"mana":{"mana":50}
+    "Flame": {"Attack": 5, "Defense": 0},
+    "Shield": {"Attack": 0, "Defense": 5},
+    "Storm": {"Attack": 3, "Defense": 2},
+    "Healing": {"HP Regen": 5}
 }
