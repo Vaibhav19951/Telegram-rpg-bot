6b65de72d76d8013ce5975785f6f9333ce59de6e
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-14 22:29:32 +0530

    Update bosses.py

--- data/bosses.py
+++ data/bosses.py
@@ -1,9 +1,5 @@
-
-bosses = {
-"E_goblin":{"hp":1000},
-"D_orc":{"hp":2000},
-"C_beast":{"hp":4000},
-"B_demon":{"hp":6000},
-"A_dragon":{"hp":9000},
-"S_monarch":{"hp":15000}
-}
+bosses = [
+    {"name": "Goblin Chief", "HP": 50, "Attack": 8},
+    {"name": "Dark Mage", "HP": 70, "Attack": 12},
+    {"name": "Stone Golem", "HP": 100, "Attack": 6}
+]
