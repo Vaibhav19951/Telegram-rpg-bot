096d4788e1709ac24c01781c8733e15aac733d11
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-16 22:50:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-# config.py
 TOKEN = "7990370974:AAFBBOIOneO3cbN4RVWlfGrgWuLkGT5gZsY"
+
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_NAME = "rpgbot"
+DB_USER = "postgres"
+DB_PASSWORD = "your_password"
+
+DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
