4ff26272819993a93f452615574dbec154df7671
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-17 12:19:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = "7990370974:AAFBBOIOneO3cbN4RVWlfGrgWuLkGT5gZsY"
 
-DATABASE_URL = "postgresql://postgres:vk@localhost:5432/rpgbot"
+DATABASE_URL = "postgresql://neondb_owner:npg_Fe2PraA6zJuW@ep-shiny-dream-adn9ascd-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
