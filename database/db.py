0f10c20a52f934ed25af5dd47eea5ecd40d4b83a
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-03-18 13:53:35 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,3 +1,16 @@
-users = {}
+import json
+import os
 
-print("🔥 DB LOADED")
+FILE = "data.json"
+
+# load data
+if os.path.exists(FILE):
+    with open(FILE, "r") as f:
+        users = json.load(f)
+else:
+    users = {}
+
+
+def save_data():
+    with open(FILE, "w") as f:
+        json.dump(users, f)
